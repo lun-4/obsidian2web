53043d568f9f18b4c1b35498565852badcc418d2
Luna <git@l4.pm>
2021-12-19 13:58:57 -0300

    open tree on load based on path

--- src/resources/main.js
+++ src/resources/main.js
@@ -1,9 +1,33 @@
-window.onload = function () {
+const treeMap = {};
+
+function registerTreeToggles() {
   const toggler = document.getElementsByClassName("caret");
   for (const element of toggler) {
+    treeMap[element.innerText] = element;
     element.addEventListener("click", function () {
       this.parentElement.querySelector(".nested").classList.toggle("active");
       this.classList.toggle("caret-down");
     });
   }
+}
+
+// Based on document.location, open the necessary tree buttons
+function openTreeFromPath() {
+  const path = decodeURIComponent(window.location.pathname);
+  for (let raw_component of path.split("/")) {
+    if (!raw_component) continue;
+    if (raw_component.endsWith(".html")) {
+      raw_component = raw_component.slice(0, -5);
+    }
+    const component = raw_component;
+    const element = treeMap[component];
+    if (!element) continue;
+    element.parentElement.querySelector(".nested").classList.toggle("active");
+    element.classList.toggle("caret-down");
+  }
+}
+
+window.onload = function () {
+  registerTreeToggles();
+  openTreeFromPath();
 };
